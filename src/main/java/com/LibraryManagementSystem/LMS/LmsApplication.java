ad66e2781ba00c13177e84776563d7cf5dc1c314
Sameeh Hussein <sameehhussein02@gmail.com>
2024-06-02 02:59:55 +0300

    Initial commit

---
+++ src/main/java/com/LibraryManagementSystem/LMS/LmsApplication.java
@@ -0,0 +1,13 @@
+package com.LibraryManagementSystem.LMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LibraryManagementSystem/LMS/LmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LibraryManagementSystem.LMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
