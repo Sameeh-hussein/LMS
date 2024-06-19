bd45fb95574fe7f123d861694e10059873dd2f5c
Sameeh Hussein <sameehhussein02@gmail.com>
2024-06-19 18:24:28 +0300

    Rename UserControllerTest to AuthControllerTest and Update it

--- src/test/java/com/LibraryManagementSystem/LMS/controllers/AuthControllerTest.java
+++ src/test/java/com/LibraryManagementSystem/LMS/controllers/AuthControllerTest.java
@@ -3,7 +3,9 @@ package com.LibraryManagementSystem.LMS.controllers;
 import com.LibraryManagementSystem.LMS.TestDataUtil;
 import com.LibraryManagementSystem.LMS.auth.LoginRequest;
 import com.LibraryManagementSystem.LMS.auth.SignupRequest;
+import com.LibraryManagementSystem.LMS.domain.Role;
 import com.LibraryManagementSystem.LMS.domain.User;
+import com.LibraryManagementSystem.LMS.repositories.RoleRepository;
 import com.LibraryManagementSystem.LMS.repositories.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -18,11 +20,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.Optional;
+
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @AutoConfigureMockMvc
-public class UserControllerTest {
+public class AuthControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -36,9 +40,15 @@ public class UserControllerTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     @Test
     public void testThatLoginReturnsHttp200OkWhenUserExist() throws Exception {
         User user = testDataUtil.createUserForTest();
+        Role role = testDataUtil.createRoleForTest();
+        roleRepository.save(role);
+        user.setRole(role);
 
         userRepository.save(user);
 
@@ -47,7 +57,7 @@ public class UserControllerTest {
         String loginRequestJson = objectMapper.writeValueAsString(loginRequest);
 
         mockMvc.perform(
-                MockMvcRequestBuilders.post("/login")
+                MockMvcRequestBuilders.post("/api/auth/login")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(loginRequestJson)
         ).andExpect(
@@ -62,7 +72,7 @@ public class UserControllerTest {
         String loginRequestJson = objectMapper.writeValueAsString(loginRequest);
 
         mockMvc.perform(
-                MockMvcRequestBuilders.post("/login")
+                MockMvcRequestBuilders.post("/api/auth/login")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(loginRequestJson)
         ).andExpect(
@@ -71,24 +81,69 @@ public class UserControllerTest {
     }
 
     @Test
-    public void testThatSignupReturnsHttp201CreatedWhenValidRegister() throws Exception {
+    public void testThatRegisterMemberReturnsHttp201CreatedWhenValidRegister() throws Exception {
         SignupRequest signupRequest = testDataUtil.createSignupRequestForTest();
+        Role memberRole = Role.builder()
+                .name("ROLE_MEMBER")
+                .build();
+
+        roleRepository.save(memberRole);
 
         String signupRequestJson = objectMapper.writeValueAsString(signupRequest);
 
         mockMvc.perform(
-                MockMvcRequestBuilders.post("/signup")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(signupRequestJson)
+                MockMvcRequestBuilders.post("/api/auth/register-member")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(signupRequestJson)
         ).andExpect(
                 MockMvcResultMatchers.status().isCreated()
         );
     }
 
     @Test
-    public void testThatSignupReturnsHttp409ConflictWhenUnValidRegister() throws Exception {
-        User user = testDataUtil.createUserForTest();
+    public void testThatRegisterLibrarianReturnsHttp201CreatedWhenValidRegister() throws Exception {
+        SignupRequest signupRequest = testDataUtil.createSignupRequestForTest();
+        Role librarianRole = Role.builder()
+                .name("ROLE_LIBRARIAN")
+                .build();
+        roleRepository.save(librarianRole);
 
+        String signupRequestJson = objectMapper.writeValueAsString(signupRequest);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/auth/register-librarian")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(signupRequestJson)
+        ).andExpect(
+                MockMvcResultMatchers.status().isCreated()
+        );
+    }
+
+    @Test
+    public void testThatRegisterAdminReturnsHttp201CreatedWhenValidRegister() throws Exception {
+        SignupRequest signupRequest = testDataUtil.createSignupRequestForTest();
+        Role adminRole = Role.builder()
+                .name("ROLE_ADMIN")
+                .build();
+        roleRepository.save(adminRole);
+
+        String signupRequestJson = objectMapper.writeValueAsString(signupRequest);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/auth/register-admin")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(signupRequestJson)
+        ).andExpect(
+                MockMvcResultMatchers.status().isCreated()
+        );
+    }
+
+    @Test
+    public void testThatRegisterReturnsHttp409ConflictWhenUserAlreadyExists() throws Exception {
+        User user = testDataUtil.createUserForTest();
+        Role role = testDataUtil.createRoleForTest();
+        roleRepository.save(role);
+        user.setRole(role);
         userRepository.save(user);
 
         SignupRequest signupRequest = testDataUtil.createSignupRequestForTest();
@@ -96,12 +151,11 @@ public class UserControllerTest {
         String signupRequestJson = objectMapper.writeValueAsString(signupRequest);
 
         mockMvc.perform(
-                MockMvcRequestBuilders.post("/signup")
+                MockMvcRequestBuilders.post("/api/auth/register-member")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(signupRequestJson)
         ).andExpect(
                 MockMvcResultMatchers.status().isConflict()
         );
     }
-
 }
