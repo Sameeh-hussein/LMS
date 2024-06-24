687bf9357a515941beeeaf95ff98c037e155fa18
Sameeh Hussein <sameehhussein02@gmail.com>
2024-06-24 22:49:43 +0300

    Add Category Not Found Exception

---
+++ src/main/java/com/LibraryManagementSystem/LMS/exceptions/CategoryNotFoundException.java
@@ -0,0 +1,8 @@
+package com.LibraryManagementSystem.LMS.exceptions;
+
+public class CategoryNotFoundException extends RuntimeException {
+
+    public CategoryNotFoundException(String message) {
+        super(message);
+    }
+}
