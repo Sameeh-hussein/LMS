1dac5e2dc2019c50c1246bc11627b48d3127fd75
Sameeh Hussein <sameehhussein02@gmail.com>
2024-07-04 23:49:16 +0300

    Add book exist by title, and isbn methods to book repository

--- src/main/java/com/LibraryManagementSystem/LMS/repositories/BookRepository.java
+++ src/main/java/com/LibraryManagementSystem/LMS/repositories/BookRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+    boolean existsByIsbn(String isbn);
+
+    boolean existsByTitle(String title);
 }
