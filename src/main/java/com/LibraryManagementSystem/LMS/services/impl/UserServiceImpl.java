18dd70b25c84d81c8812857a02df85893f860c2e
Sameeh Hussein <sameehhussein02@gmail.com>
2024-07-09 02:35:53 +0300

    Add InternalAuthenticationServiceException handling in authenticateUser method.

--- src/main/java/com/LibraryManagementSystem/LMS/services/impl/UserServiceImpl.java
+++ src/main/java/com/LibraryManagementSystem/LMS/services/impl/UserServiceImpl.java
@@ -15,6 +15,7 @@ import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -32,17 +33,22 @@ public class UserServiceImpl implements UserService {
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
+    private final UserRequestMapper userRequestMapper;
     private final UserReturnMapper userReturnMapper;
     private final AuthenticationManager authenticationManager;
 
     @Override
     public String authenticateUser(LoginRequest request) {
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        request.getEmail(),
-                        request.getPassword()
-                )
-        );
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            request.getEmail(),
+                            request.getPassword()
+                    )
+            );
+        } catch (InternalAuthenticationServiceException e) {
+            throw new UserNotFoundException("User not found with email: " + request.getEmail());
+        }
 
         User user = userRepository.findByEmail(request.getEmail()).orElseThrow(() ->
                 new UserNotFoundException("User not found with email: " + request.getEmail()));
@@ -65,14 +71,10 @@ public class UserServiceImpl implements UserService {
             throw new RoleNotFoundException("Role not found with name: " + roleName);
         }
 
-        User user = User.builder()
-                .email(request.getEmail())
-                .password(passwordEncoder.encode(request.getPassword()))
-                .role(role.get())
-                .firstName(request.getFirstName())
-                .lastName(request.getLastName())
-                .createdAt(LocalDateTime.now())
-                .build();
+        User user = userRequestMapper.mapFrom(request);
+        user.setRole(role.get());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setCreatedAt(LocalDateTime.now());
 
         userRepository.save(user);
     }
