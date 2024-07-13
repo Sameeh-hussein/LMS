ea40ec6dfd0cde80869a0be47e1abc951739ca29
Sameeh Hussein <sameehhussein02@gmail.com>
2024-07-14 00:47:34 +0300

    Add test cases for Role Service Test

--- src/test/java/com/LibraryManagementSystem/LMS/Services/RoleServiceImplTest.java
+++ src/test/java/com/LibraryManagementSystem/LMS/Services/RoleServiceImplTest.java
@@ -5,6 +5,7 @@ import com.LibraryManagementSystem.LMS.domain.Role;
 import com.LibraryManagementSystem.LMS.dto.AddRoleDto;
 import com.LibraryManagementSystem.LMS.dto.ReturnRoleDto;
 import com.LibraryManagementSystem.LMS.exceptions.RoleAlreadyExistsException;
+import com.LibraryManagementSystem.LMS.exceptions.RoleNotFoundException;
 import com.LibraryManagementSystem.LMS.mappers.impl.RoleRequestMapper;
 import com.LibraryManagementSystem.LMS.mappers.impl.RoleReturnMapper;
 import com.LibraryManagementSystem.LMS.repositories.RoleRepository;
@@ -19,6 +20,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -46,11 +48,13 @@ public class RoleServiceImplTest {
         Role role = Role.builder().name("ROLE_MEMBER").build();
 
         when(roleRepository.existsByName(addRoleDto.getName())).thenReturn(false);
-
         when(roleRequestMapper.mapFrom(addRoleDto)).thenReturn(role);
+        when(roleRepository.save(role)).thenReturn(role);
 
         underTest.addNewRole(addRoleDto);
 
+        verify(roleRepository, times(1)).save(role);
+        verify(roleRequestMapper, times(1)).mapFrom(addRoleDto);
         verify(roleRepository, times(1)).save(role);
     }
 
@@ -67,8 +71,8 @@ public class RoleServiceImplTest {
         assertNotNull(roleAlreadyExistsException);
 
         verify(roleRepository, times(1)).existsByName(addRoleDto.getName());
-
         verify(roleRequestMapper, never()).mapFrom(any(AddRoleDto.class));
+        verify(roleRepository, never()).save(any(Role.class));
     }
 
     @Test
@@ -80,17 +84,46 @@ public class RoleServiceImplTest {
         ReturnRoleDto returnRoleDto2 = ReturnRoleDto.builder().name("ROLE_ADMIN").build();
 
         when(roleRepository.findAll()).thenReturn(Arrays.asList(role1, role2));
-
         when(roleReturnMapper.mapTo(role1)).thenReturn(returnRoleDto1);
-
         when(roleReturnMapper.mapTo(role2)).thenReturn(returnRoleDto2);
 
         List<ReturnRoleDto> roles = underTest.findAll();
 
         assertEquals(2, roles.size());
-
         assertEquals("ROLE_MEMBER", roles.get(0).getName());
-
         assertEquals("ROLE_ADMIN", roles.get(1).getName());
+
+        verify(roleRepository, times(1)).findAll();
+        verify(roleReturnMapper, times(1)).mapTo(role1);
+        verify(roleReturnMapper, times(1)).mapTo(role2);
     }
+
+    @Test
+    public void testThatFindByIdReturnsRoleWhenRoleExists() {
+        Role role = Role.builder().id(1L).name("ROLE_MEMBER").build();
+
+        ReturnRoleDto returnRoleDto = ReturnRoleDto.builder().name("ROLE_MEMBER").build();
+
+        when(roleRepository.findById(1L)).thenReturn(Optional.of(role));
+        when(roleReturnMapper.mapTo(role)).thenReturn(returnRoleDto);
+
+        ReturnRoleDto result = underTest.findRoleById(1L);
+
+        assertNotNull(result);
+        assertEquals("ROLE_MEMBER", result.getName());
+
+        verify(roleRepository, times(1)).findById(1L);
+        verify(roleReturnMapper, times(1)).mapTo(role);
+    }
+    @Test
+    public void testThatFindByIdThrowRoleNotFoundExceptionWhenRoleNotExists() {
+        when(roleRepository.findById(999L)).thenReturn(Optional.empty());
+
+        assertThrows(RoleNotFoundException.class, () -> underTest.findRoleById(999L));
+
+        verify(roleRepository, times(1)).findById(999L);
+        
+        verify(roleReturnMapper, times(0)).mapTo(any(Role.class));
+    }
+
 }
