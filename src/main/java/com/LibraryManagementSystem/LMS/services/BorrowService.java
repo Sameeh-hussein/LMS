f23856d2f8a063cf03b83d6359b0b6373d478416
Sameeh Hussein <sameehhussein02@gmail.com>
2024-07-17 18:22:39 +0300

    Add Borrow Service

---
+++ src/main/java/com/LibraryManagementSystem/LMS/services/BorrowService.java
@@ -0,0 +1,4 @@
+package com.LibraryManagementSystem.LMS.services;
+
+public interface BorrowService {
+}
---
+++ src/main/java/com/LibraryManagementSystem/LMS/services/impl/BorrowServiceImpl.java
@@ -0,0 +1,8 @@
+package com.LibraryManagementSystem.LMS.services.impl;
+
+import com.LibraryManagementSystem.LMS.services.BorrowService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BorrowServiceImpl implements BorrowService {
+}
